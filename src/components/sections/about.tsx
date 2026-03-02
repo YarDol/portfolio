47c81ed435ad1ebeb595ce0ca83d84d6c6d31bc4
YarDo <yardolhushyn@gmail.com>
2026-03-03 00:15:37 +0100

    fix box right border

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -110,6 +110,8 @@ export function About() {
             <div className="absolute inset-x-0 top-0 h-16 bg-linear-to-b from-background to-transparent z-10 pointer-events-none" />
             <div className="absolute inset-x-0 bottom-0 h-16 bg-linear-to-t from-background to-transparent z-10 pointer-events-none" />
 
+            <div className="absolute inset-y-0 right-0 w-16 bg-linear-to-l from-background to-transparent z-10 pointer-events-none" />
+
             <NeuralGraph />
           </motion.div>
         </div>
